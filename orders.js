d04c1cb343edd38dd614952e4af9721bf4820a58
LAB11_USER ON WS03 <L11X09W03@LabHCI.net>
2026-09-18 08:58:13 +0800

    Added a VIP Bonus rule - alcontin.raymundo

--- orders.js
+++ orders.js
@@ -16,7 +16,11 @@ function cancelOrder(order) {
 }
 
 function calculateLoyaltyPoints(order) {
-  return Math.floor(order.total / 10);
+	let points = order.total / 10;
+	if(order.total > 100){
+		points = points * 1.5;
+	}
+  return Math.floor(points);
 }
 
 module.exports = { createOrder, applyDiscount, cancelOrder, calculateLoyaltyPoints };
